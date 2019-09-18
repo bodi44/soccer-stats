4036d272d64f552e4fbb7a667bdeb64ddebbf01d
bodi44 <stogrinec.bodia@gmail.com>
2019-09-18 13:24:13 +0300

    Rewrite modules to composition

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,10 @@
 import fs from 'fs';
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+export class CsvFileReader {
+  data: string[][] = [];
 
   constructor(public filename: string) {
   }
-
-  abstract mapRow(row: string[]): T
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -15,7 +13,6 @@ export abstract class CsvFileReader<T> {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      })
-      .map(this.mapRow);
+      });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-import { MatchReader } from './inheritance/MatchReader';
-import { MatchResults } from './MatchResults';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('./src/data/football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('./src/data/football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-const dateOfFirstMatch = reader.data[0][0];
-
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games.`);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
